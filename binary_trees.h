156c05537c765191da92921aa73f2f9cb7f84443
solotole <solomontoleak47@gmail.com>
2024-04-09 03:55:58 -0700

    in order binary tree traversal

--- binary_trees.h
+++ binary_trees.h
@@ -38,5 +38,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+void pre_order(const binary_tree_t *root, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void in_order(const binary_tree_t *node, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
