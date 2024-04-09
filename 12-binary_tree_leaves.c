56f17cf26a5f1513deb17fb3c3837e6ff4c0043f
solotole <solomontoleak47@gmail.com>
2024-04-08 23:28:41 -0700

    is leaves?

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,9 +9,10 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves;
+	size_t leaves = 0;
+
 	if (tree == NULL)
-		leaves = 0;
+		return (leaves);
 	else if (tree->left != NULL && tree->right != NULL)
 		leaves = 2;
 	else if ((tree->left != NULL && tree->right == NULL)
