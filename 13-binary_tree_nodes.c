776e539a22866cb9764feb835a86ab63615c1cff
solotole <solomontoleak47@gmail.com>
2024-04-10 02:52:37 -0700

    counting nodes with atleast one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * node_count - function to count nodes except leaves
+ * @root: root node of the tree or subtree
+ *
+ * Return: return 0 if NULL or count otherwise
+ */
 size_t node_count(const binary_tree_t *root)
 {
 	size_t left, right, total;
@@ -13,7 +19,12 @@ size_t node_count(const binary_tree_t *root)
 	total = left + right + 1;
 	return (total);
 }
-
+/**
+ * binary_tree_nodes - function that counts nodes
+ * except leaf nodes
+ * @tree: root node of tree
+ * Return: return the nodes count
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count;
