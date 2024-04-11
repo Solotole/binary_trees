0acf0e49cfbb41e33bdbd625dcb18957dd46ab9f
solotole <solomontoleak47@gmail.com>
2024-04-11 02:45:59 -0700

    is full?

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,29 @@
 #include "binary_trees.h"
+
+/**
+ * full_tree - function to confirm for full tree
+ * characteristics
+ * @node: root node of the tree
+ * Return: return 0 if not full tree or 1 otherwise
+ */
+int full_tree(const binary_tree_t *node)
+{
+	int left, right;
+
+	if (!node)
+		return (1);
+	if ((node->left == NULL && node->right != NULL) ||
+			 (node->left != NULL && node->right == NULL))
+		return (0);
+	left = full_tree(node->left);
+	right = full_tree(node->right);
+	if ((left == 1 && right == 0) || (left == 0 && right == 1))
+		return (0);
+	if (left == 1 && right == 1)
+		return (1);
+	return (0);
+}
+
 /**
  * binary_tree_is_full - function that checks if a binary tree is full
  * @tree: pointer to the root node of the tree to check
@@ -8,23 +33,9 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int value;
-	const binary_tree_t *node1, *node2;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	else if (tree->right != NULL && tree->left != NULL)
-	{
-		node1 = tree->right;
-		node2 = tree->left;
-		if ((node2->right != NULL || node2->left != NULL) &&
-				 (node1->right != NULL || node1->left != NULL))
-			value = 0;
-		else if (node2->right == NULL && node2->left == NULL &&
-				 node1->left == NULL && node1->right == NULL)
-			value = 1;
-	}
-	else if ((tree->right == NULL && tree->left != NULL) ||
-			 (tree->right != NULL && tree->left == NULL))
-		value = 0;
+	value = full_tree(tree);
 	return (value);
 }
