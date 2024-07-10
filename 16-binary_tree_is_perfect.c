a23475e99f511e477c6dcb1229a4b82da2ff2edc
Solotole <solomontoleak47@gmail.com>
2024-07-10 11:31:32 +0100

    is perfect?

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,86 +1,59 @@
 #include "binary_trees.h"
 
 /**
- * depth - function to find node depth
- * @tree: tree to check for it's depth
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
  *
- * Return: return node depth
+ * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
  */
-int depth(const binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	const binary_tree_t *node = tree;
-	int to_root = 0;
-
-	while (node)
-	{
-		to_root++;
-		node = node->parent;
-	}
-	return (to_root);
-}
-
-/**
- * pre_order_leaf - preorder traversal
- * @tree: tree to traverse through
- * @height: height of the tree
- *
- * Return: return 0 if not perfect or 1 otherwise
- */
-int pre_order_leaf(const binary_tree_t *tree, int height)
-{
-	int left, depth_to_root, right;
-
-	depth_to_root = depth(tree);
-	if (!tree->left && !tree->right && depth_to_root == height)
+	if (node != NULL && node->left == NULL && node->right == NULL)
 		return (1);
-	if (!tree->left && !tree->right && depth_to_root != height)
-		return (0);
-	if ((!tree->left && tree->right) || (!tree->right && tree->left))
-		return (0);
-	left = pre_order_leaf(tree->left, height);
-	right = pre_order_leaf(tree->right, height);
-	if ((left == 0 && right == 1) || (right == 0 && left == 1))
-		return (0);
-	if (right == 1 && left == 1)
-		return (1);
-	return (1);
-}
-
-/**
- * height - function to check height of the tree
- * @tree: tree to find it's height
- *
- * Return: return 0 if tree is NULL or tree height
- * otherwise
- */
-int height(const binary_tree_t *tree)
-{
-	int left, right;
-
-	if (!tree)
-		return (0);
-	left = height(tree->left);
-	right = height(tree->right);
-	if (left >= right)
-		return (left + 1);
-	if (right > left)
-		return (right + 1);
 	return (0);
 }
 
 /**
- * binary_tree_is_perfect - function to check if tree is
- * perfect
- * @tree: tree to check
- * Return: return 1 if perfet and 0 otherwise
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of the tree. If tree is NULL, return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int leaf,tree_height;
+	binary_tree_t *l, *r;
 
 	if (tree == NULL)
 		return (0);
-	tree_height = height(tree);
-	leaf = pre_order_leaf(tree, tree_height);
-	return (leaf);
+	l = tree->left;
+	r = tree->right;
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	if (l == NULL || r == NULL)
+		return (0);
+	if (binary_tree_height(l) == binary_tree_height(r))
+	{
+		if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
+			return (1);
+	}
+	return (0);
 }
