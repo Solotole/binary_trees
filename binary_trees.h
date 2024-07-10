10ec7f0a4a4721f030c46a11d5062241c603b94f
Solotole <solomontoleak47@gmail.com>
2024-07-10 11:38:22 +0100

    is complete?

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
+int binary_tree_is_complete(const binary_tree_t *tree);
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
